4abc28ead547f2af04b5351c1621b93df14ecac1
Gustavo Lira <gustavolira8813@gmail.com>
2021-09-24 22:13:36 -0300

    fix background

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,9 +1,9 @@
 import "./styles.css";
 function Home() {
   return (
-    <div>
-      <h3> The Big World</h3>
-      <img src="https://lh3.googleusercontent.com/proxy/Ggdefx29dPHGAb2cWjxS1HXcOkId46yEo7SoeMYmX6gdW6MB0-c2HPG5Rjkk-DK3Pmlddz0XX_uOIeBdI70WpEeRTCTE6rhnd5d85feoFXcUoq0KYSoz0YJViGDfpaCWyiNfycv5te7D2Gw" />
+    <div className="home">
+      <h3 className="home_title"> The Big World</h3>
+      {/* <img src="https://ak.picdn.net/shutterstock/videos/5117402/thumb/1.jpg" /> */}
     </div>
   );
 }
